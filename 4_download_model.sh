a439b7b3d251b926c6a0c75e32cfa282630f8dff
Vivek <vivek@pipeshift.com>
2025-07-04 21:24:38 +0530

    Create 4_download_model.sh

---
+++ 4_download_model.sh
@@ -0,0 +1,20 @@
+# HF_MODEL_PATH="meta-llama/Llama-3.1-8B-Instruct"
+# HF_MODEL_PATH="meta-llama/Llama-3.2-1B-Instruct"
+# HF_MODEL_PATH="deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
+HF_MODEL_PATH="mistralai/Mixtral-8x7B-Instruct-v0.1"
+HF_USERNAME=
+HF_TOKEN=""
+
+# git config --global credential.helper store
+# huggingface-cli login --token $HF_TOKEN --add-to-git-credential
+
+
+# Install git-lfs if needed
+sudo apt-get update -y && sudo apt-get install git-lfs -y --no-install-recommends
+git lfs install
+
+# Clone the Hugging Face model repository
+HF_MODEL_DIR=~/tensorrt/hf_model
+[ -d $HF_MODEL_DIR ] && rm -rf $HF_MODEL_DIR
+mkdir -p $HF_MODEL_DIR && cd $HF_MODEL_DIR
+GIT_ASKPASS=echo git clone https://$HF_USERNAME:$HF_TOKEN@huggingface.co/$HF_MODEL_PATH .
